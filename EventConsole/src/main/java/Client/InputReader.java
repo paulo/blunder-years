407c2545ca52385aab0d99a50b627e6534f77798
SimaoDias <simao.cdias@gmail.com>
2015-12-11 10:47:14 +0000

    small changes, console notification comments removed

--- EventConsole/src/main/java/Client/InputReader.java
+++ EventConsole/src/main/java/Client/InputReader.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Client;
 
 import java.util.ArrayList;
@@ -12,10 +8,6 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 import org.zeromq.ZMQ;
 
-/**
- *
- * @author SimaoDias
- */
 public class InputReader extends Thread{
     
     static ZMQ.Socket socket;
