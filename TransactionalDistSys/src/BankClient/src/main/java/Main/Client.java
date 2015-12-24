a2b29533d726c5bc782675e35a9769e369c2e1c9
SimaoDias <simao.cdias@gmail.com>
2015-12-24 14:22:50 +0000

    old code

---
+++ TransactionalDistSys/src/BankClient/src/main/java/Main/Client.java
@@ -0,0 +1,21 @@
+package Main;
+
+import Model.AccountIf;
+import Model.BankIf;
+import Model.TransactionIf;
+import java.rmi.Naming;
+
+public class Client {
+    public static void main(String[] args) throws Exception {
+        BankIf bank = (BankIf) Naming.lookup("//localhost/firstbank");
+
+        AccountIf acc = bank.getAccount(00100002);
+        System.out.println("Current Balance: " + acc.getBalance());
+        
+        TransactionIf trans = bank.makeTransaction();
+        trans.deposit(100, acc);
+        System.out.println("We made a deposit of 100");
+        System.out.println("Current Balance: " + acc.getBalance());
+    }
+    
+}
